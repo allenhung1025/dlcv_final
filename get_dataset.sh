bae620754dbadd982f85738900b646e7b3af7cc5
allenhung1025 <hungtungming@gmail.com>
2022-01-04 11:46:14 +0800

    modify link

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,5 +1,5 @@
 # Download dataset
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1IYWPK8h9FWyo0p4-SCAatLGy0l5omQaw' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1IYWPK8h9FWyo0p4-SCAatLGy0l5omQaw" -O food_data.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1VNuA9NLE7Qc_Xs3hLqnbY0sXhzBrPiPK' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VNuA9NLE7Qc_Xs3hLqnbY0sXhzBrPiPK" -O food_data.zip && rm -rf /tmp/cookies.txt
 
 # Unzip the downloaded zip file
 unzip ./food_data.zip
