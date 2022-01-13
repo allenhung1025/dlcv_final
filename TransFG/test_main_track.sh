a7a0488ee474cd713680bc9bc56d62135f966826
allenhung1025 <hungtungming@gmail.com>
2022-01-13 23:00:21 +0800

    test sh

--- TransFG/test_main_track.sh
+++ TransFG/test_main_track.sh
@@ -1,1 +1,1 @@
-python3 test.py --sample_csv ../food_data/testcase/sample_submission_main_track.csv --output_csv output_main_smooth_track.csv --model_path ./output_smoothing_0.1/sample_run_checkpoint.bin
+python3 test.py --sample_csv ../food_data/testcase/sample_submission_main_track.csv --output_csv output_main_track.csv --model_path ./sample_run_checkpoint.bin
